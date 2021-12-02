a359237d37833c5fa9af54d5d9ac70c089a9c6dd
TobsenD <tobias.dols@outlook.com>
2021-12-02 10:45:31 +0100

    Day two task two

--- AoC02/main.go
+++ AoC02/main.go
@@ -11,7 +11,13 @@ import (
 
 func main() {
 
-	file, err := os.Open("./input0201.txt")
+	//task01()
+	task02()
+
+}
+
+func task01() {
+	file, err := os.Open("./input02.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -43,7 +49,43 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+}
 
+func task02() {
+	file, err := os.Open("./input02.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var hor int
+	var vert int
+	var aim int
+
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		split := strings.Split(line, " ")
+
+		switch {
+		case split[0] == "forward":
+			hor += convertInt(split[1])
+			vert += aim * convertInt(split[1])
+		case split[0] == "up":
+			aim -= convertInt(split[1])
+		case split[0] == "down":
+			aim += convertInt(split[1])
+		}
+
+	}
+
+	fmt.Println(vert * hor)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func convertInt(s string) int {
