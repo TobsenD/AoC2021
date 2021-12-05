9d80c7ea620ed5c78dac27cfab079eea1849bfd1
TobsenD <tobias.dols@outlook.com>
2021-12-05 11:59:33 +0100

    Day five task two

--- AoC05/main.go
+++ AoC05/main.go
@@ -11,8 +11,8 @@ import (
 
 func main() {
 
-	task01()
-	//task02()
+	//task01()
+	task02()
 
 }
 
@@ -63,8 +63,6 @@ func task01() {
 		}
 	}
 
-	//fmt.Println(ventMap)
-
 	var ventCount int
 
 	for i := 0; i < 1000; i++ {
@@ -80,6 +78,94 @@ func task01() {
 }
 
 func task02() {
+	file, err := os.Open("./input05.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	var ventMap [1000][1000]int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		array := regexp.MustCompile("[\\,\\->\\s]+").Split(line, -1)
+		var ventPosition []int
+		for _, value := range array {
+			ventPosition = append(ventPosition, convertInt(value))
+		}
+
+		if ventPosition[0] == ventPosition[2] {
+			if ventPosition[1] < ventPosition[3] {
+				for i := ventPosition[1]; i <= ventPosition[3]; i++ {
+					ventMap[i][ventPosition[0]]++
+				}
+			} else {
+				for i := ventPosition[1]; i >= ventPosition[3]; i-- {
+					ventMap[i][ventPosition[0]]++
+				}
+			}
+		} else if ventPosition[1] == ventPosition[3] {
+			if ventPosition[0] < ventPosition[2] {
+				for i := ventPosition[0]; i <= ventPosition[2]; i++ {
+					ventMap[ventPosition[1]][i]++
+				}
+			} else {
+				for i := ventPosition[0]; i >= ventPosition[2]; i-- {
+					ventMap[ventPosition[1]][i]++
+				}
+			}
+		} else if ((ventPosition[0]-ventPosition[2])+(ventPosition[1]-ventPosition[3]))%2 == 0 {
+
+			if ventPosition[1] < ventPosition[3] {
+				for i := ventPosition[1]; i <= ventPosition[3]; i++ {
+					if ventPosition[0] < ventPosition[2] {
+						for j := ventPosition[0]; j <= ventPosition[2]; j++ {
+							ventMap[i][j]++
+							i++
+						}
+					} else {
+						for j := ventPosition[0]; j >= ventPosition[2]; j-- {
+							ventMap[i][j]++
+							i++
+						}
+					}
+				}
+			} else {
+				for i := ventPosition[1]; i >= ventPosition[3]; i-- {
+					if ventPosition[0] < ventPosition[2] {
+						for j := ventPosition[0]; j <= ventPosition[2]; j++ {
+							ventMap[i][j]++
+							i--
+						}
+					} else {
+						for j := ventPosition[0]; j >= ventPosition[2]; j-- {
+							ventMap[i][j]++
+							i--
+						}
+					}
+				}
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	//fmt.Println(ventMap)
+	var ventCount int
+
+	for i := 0; i < 1000; i++ {
+		for j := 0; j < 1000; j++ {
+			if ventMap[i][j] > 1 {
+				ventCount++
+			}
+		}
+	}
+
+	fmt.Println(ventCount)
 
 }
 
