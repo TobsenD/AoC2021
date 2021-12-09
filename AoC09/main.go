9d4f763aea82fd22ab4db4f2a9bf8e78ef8aa4a3
TobsenD <tobias.dols@outlook.com>
2021-12-09 10:19:02 +0100

    Day nine, task two

--- AoC09/main.go
+++ AoC09/main.go
@@ -5,14 +5,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 func main() {
 
-	task01()
-	//task02()
+	//task01()
+	task02()
 
 }
 
@@ -78,7 +79,102 @@ func task01() {
 }
 
 func task02() {
+	file, err := os.Open("./input09.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
 
+	//Prepare Data
+	matrix := make([][]int, 0)
+	for scanner.Scan() {
+		row := make([]int, 0)
+		line := scanner.Text()
+		slc := strings.Split(line, "")
+		for x := range slc {
+			row = append(row, convertInt(slc[x]))
+		}
+		matrix = append(matrix, row)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	//Work with data
+	mHeight := len(matrix)
+	mWidth := len(matrix[0])
+
+	countList := make([]int, 0)
+
+	for y, row := range matrix {
+		for x, col := range row {
+			left, up, right, down := x-1, y-1, x+1, y+1
+
+			isLowPoint := true
+			if left >= 0 {
+				isLowPoint = col < matrix[y][left] && isLowPoint
+			}
+
+			if up >= 0 {
+				isLowPoint = col < matrix[up][x] && isLowPoint
+			}
+
+			if right < mWidth {
+				isLowPoint = col < matrix[y][right] && isLowPoint
+			}
+
+			if down < mHeight {
+				isLowPoint = col < matrix[down][x] && isLowPoint
+			}
+
+			if isLowPoint {
+				count := calcBasinSize(matrix, x, y, 1, make(map[string]bool))
+				countList = append(countList, count)
+			}
+		}
+	}
+
+	sort.Ints(countList)
+	countlen := len(countList)
+	fmt.Println(countList[countlen-1] * countList[countlen-2] * countList[countlen-3])
+}
+
+func calcBasinSize(matrix [][]int, x int, y int, count int, visited map[string]bool) int {
+	position := fmt.Sprintf("%d,%d", y, x)
+	if _, isVisited := visited[position]; isVisited {
+		return count - 1
+	} else {
+		visited[position] = true
+	}
+
+	if matrix[y][x] == 9 {
+		return count - 1
+	}
+
+	mHeight := len(matrix)
+	mWidth := len(matrix[0])
+
+	left, up, right, down := x-1, y-1, x+1, y+1
+
+	if left >= 0 {
+		count = calcBasinSize(matrix, left, y, count+1, visited)
+	}
+
+	if up >= 0 {
+		count = calcBasinSize(matrix, x, up, count+1, visited)
+	}
+
+	if right < mWidth {
+		count = calcBasinSize(matrix, right, y, count+1, visited)
+	}
+
+	if down < mHeight {
+		count = calcBasinSize(matrix, x, down, count+1, visited)
+	}
+
+	return count
 }
 
 func convertInt(s string) int {
